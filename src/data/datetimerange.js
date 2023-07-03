25344b99ec4f4a4196eb639afe672307be54b856
Sören Meier <soeren@s-me.ch>
2023-07-04 00:23:55 +0200

    aad DateTimeRange::{toStrShortTime, toStrDateShortTime}

--- src/data/datetimerange.js
+++ src/data/datetimerange.js
@@ -2,12 +2,19 @@
 import { padZero } from './../util.js';
 
 export default class DateTimeRange {
+	/// expects
+	/// from: DateTime
+	/// to: DateTime
 	constructor(from, to) {
 		this.from = from;
 		this.to = to;
 	}
 
-	// returns 10.10.2020 - 10.10.2020
+	// returns
+	// 10.10.2020 - 10.12.2023
+	// 10.10 - 10.12.2020
+	// 11 - 10.10.2020
+	// 10.10.2020
 	toStrDate() {
 		const f = this.from;
 		const t = this.to;
@@ -22,5 +29,46 @@ export default class DateTimeRange {
 			return padZero(f.date) + ' - ' + t.toStrDate();
 
 		return t.toStrDate();
+	}
+
+	// returns 10:30 - 14:40
+	// returns 10:30
+	toStrShortTime() {
+		const f = this.from.toStrShortTime();
+		const t = this.to.toStrShortTime();
+
+		if (f !== t)
+			return f + ' - ' + t;
+		return f;
+	}
+
+	// returns 03.07.2023 18:00 - 04.07.2024 18:00
+	// returns 04.08 18:00 - 03.07.2023 18:00
+	// returns 04.07 18:00 - 05.07.2023 18:00
+	// returns 18:00 - 19:35 05.07.2023
+	toStrDateShortTime() {
+		const f = this.from;
+		const t = this.to;
+
+		if (f.year !== t.year) {
+			const from = f.toStrDate() + ' ' + f.toStrShortTime();
+			const to = t.toStrDate() + ' ' + t.toStrShortTime();
+			return from + ' - ' + to;
+		}
+
+		if (f.month !== t.month || f.date !== t.date) {
+			const from = f.toStrShortDate() + ' ' + f.toStrShortTime();
+			const to = t.toStrDate() + ' ' + t.toStrShortTime();
+			return from + ' - ' + to;
+		}
+
+		// date the same
+		if (f.hours !== t.hours || f.minutes !== t.minutes) {
+			const from = f.toStrDate() + ' ' + f.toStrShortTime();
+			const to = t.toStrShortTime();
+			return from + ' - ' + to;
+		}
+
+		return f.toStrDate() + ' ' + f.toStrShortTime();
 	}
 }
