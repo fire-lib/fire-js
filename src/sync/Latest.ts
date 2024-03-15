9e6dfc1da1947abffa7f27019e5927dbdf43567f
Sören Meier <soeren@s-me.ch>
2024-03-15 21:35:43 +0100

    Add sync/Latest

---
+++ src/sync/Latest.ts
@@ -0,0 +1,48 @@
+export type LatestStart = {
+	canceled: boolean;
+	// throw an error if the function was canceled
+	done: () => void;
+};
+
+/**
+ * A class to help make async code non concurrent.
+ * Only the latest async functions get the go ahead the other ones get cancelled.
+ */
+export default class Latest {
+	private listeners: Array<(canceled: boolean) => void>;
+	private running: boolean;
+
+	constructor() {
+		this.listeners = [];
+		this.running = false;
+	}
+
+	async start(): Promise<LatestStart> {
+		let canceled = false;
+
+		if (this.running) {
+			canceled = await new Promise<boolean>(resolve => {
+				this.listeners.push(resolve);
+			});
+		}
+
+		this.running = true;
+
+		return {
+			canceled,
+			done: () => {
+				if (canceled)
+					throw new Error('calling done on a cancelled function');
+
+				// notify the latest
+				const latest = this.listeners.pop();
+				if (!latest) return;
+
+				this.listeners.forEach(resolve => resolve(true));
+				this.listeners = [];
+
+				latest(false);
+			},
+		};
+	}
+}
